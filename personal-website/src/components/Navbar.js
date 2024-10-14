f2c050d3d634cb32be4e50f3a49917991c655c25
Noah Frey <noahfrey29@gmail.com>
2024-10-14 17:07:20 -0400

    Adding button component and CSS styling

--- personal-website/src/components/Navbar.js
+++ personal-website/src/components/Navbar.js
@@ -1,17 +1,32 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
+import './Navbar.css';
+import {Button} from './Button';
 
 function Navbar() {
   const [click, setClick] = useState(false)
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+
+  window.addEventListener('resize', showButton);
+
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
             <Link to='/' className='navbar-logo'>
-                TRVL <i className='fa-solid fa-laptop-code'/>
+                My Personal Website<i className='fa-solid fa-laptop-code'/>
             </Link>
             <div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'}/>
@@ -23,6 +38,7 @@ function Navbar() {
                     </Link>
                 </li>
             </ul>
+            {button && <Button buttonStyle='btn--outline'>TBD</Button>}
         </div>
       </nav>
     </>
