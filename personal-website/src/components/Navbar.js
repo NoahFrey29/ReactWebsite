74ec884a94c2416f5b0f696c02a1799865e0aab8
Noah Frey <noahfrey29@gmail.com>
2024-10-17 16:58:47 -0400

    Fixing navbar images, a couple bugs to still work out

--- personal-website/src/components/Navbar.js
+++ personal-website/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import './Navbar.css';
 import {Button} from './Button';
@@ -18,6 +18,9 @@ function Navbar() {
     }
   };
 
+  useEffect(() => {
+    showButton();
+  }, []);
 
   window.addEventListener('resize', showButton);
 
@@ -25,7 +28,7 @@ function Navbar() {
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-            <Link to='/' className='navbar-logo'>
+            <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                 My Personal Website<i className='fa-solid fa-laptop-code'/>
             </Link>
             <div className='menu-icon' onClick={handleClick}>
@@ -36,6 +39,34 @@ function Navbar() {
                     <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                         Home
                     </Link>
+                </li>
+                    <li className='nav-item'>
+                  <Link
+                    to='/services'
+                    className='nav-links'
+                    onClick={closeMobileMenu}
+                  >
+                    Services
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link
+                    to='/products'
+                    className='nav-links'
+                    onClick={closeMobileMenu}
+                  >
+                    Products
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    to='/sign-up'
+                    className='nav-links-mobile'
+                    onClick={closeMobileMenu}
+                  >
+                    Sign Up
+                  </Link>
                 </li>
             </ul>
             {button && <Button buttonStyle='btn--outline'>TBD</Button>}
