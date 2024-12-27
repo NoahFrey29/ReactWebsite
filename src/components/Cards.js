b0e45b894f885006523de532f78fb450c8ba92d2
Noah Frey <noahfrey29@gmail.com>
2024-12-27 12:24:42 -0500

    Adding remote origin

---
+++ src/components/Cards.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
+
+function Cards() {
+  return (
+    <>
+      <section id='cards'>
+        <div id='placeholder'/>
+        <div className='cards'>
+           <h1>Check out my Work Terms and Co-op Experiences:</h1>
+              <div className='cards__container'>
+                  <div className='cards__wrapper'>
+                      <ul className='cards__items'>
+                          <CardItem
+                              src='images/work-term-one.jpg' // add standing in front of score building
+                              text='QA Automation Engineer at theScore - WT1'
+                              label='Work Term 1 - S2024'
+                              path='/work-term-one' />
+                          <CardItem
+                              src='images/thescore-office.jpeg'
+                              text='QA Automation Engineer at theScore - WT2'
+                              label='Work Term 2 - F2024'
+                              path='/work-term-two' />
+                      </ul>
+                      <ul className='cards__items'>
+                          <CardItem
+                              src='images/hpe.jpg'
+                              text='Software Developer Intern at Hewlett Packard Enterprise'
+                              label='Summer 2023 at HPE'
+                              path='/hpe' />
+                          <CardItem
+                              src='images/img-2.jpg'
+                              text='Work Terms 3, 4, 5'
+                              label='Future Work - TBD'
+                              path='/tbd' />
+                          <CardItem
+                              src='images/img-8.jpg'
+                              text='Check out my Resume!'
+                              label='My Resume'
+                              path='/resume.pdf' />
+                      </ul>
+                  </div>
+              </div>
+          </div>
+      </section>
+    </>
+  );
+}
+
+export default Cards;
---
+++ src/components/pages/TBD.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../../App.css';
+
+export default function TBD() {
+    return (
+      <div style={{
+        height: '45vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+      }}>
+        <i class="fa-solid fa-circle-exclamation" style={{
+            paddingRight: '20px',
+            fontSize: '36px',
+            color: 'red',
+        }}></i>
+        <h1 className='tbd' style={{
+            fontSize: '48px',
+            color: 'crimson',
+            textShadow: '0 0 0.5em black',
+        }}>
+            
+          Work in Progress... Come Back Later!
+        </h1>
+        <i class="fa-solid fa-circle-exclamation" style={{
+            paddingLeft: '20px',
+            fontSize: '36px',
+            color: 'red',
+        }}></i>
+      </div>
+    );
+  }
